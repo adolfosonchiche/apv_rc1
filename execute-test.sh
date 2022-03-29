405d2d911584ed2c3f69c0c881e63aebdd7e4b08
adolfosonchiche <sonadolfo36@gmail.com>
2022-03-29 11:15:46 -0600

    vsw

--- execute-test.sh
+++ execute-test.sh
@@ -21,27 +21,39 @@ log-queries
 EOF
 echo "fin"
 service dnsmasq start
+#inteface wifi  es wlo1 para mi
+ifconfig wlan0 up
+ifconfig wlan0 127.0.0.1/24 #AQUI PONER EL IP DE DEL ROUTER
+
+iptables -t nat -F
+iptables -F
+iptables -t nat -A POSTROUTING -o enp2s0 -j MASQUERADE
+iptables -A FORWARD -i wlp1s0 -o enp2s0 -j ACCEPT
+echo '1' > /proc/sys/net/ipv4/ip_forward
+
 echo "inicio conf apd"
 cat <<EOF > /etc/hostapd/hostapd.conf
 interface=wlp1s0
 driver=nl80211
 ssid=myhotspot
-hw_mode=g
-channel=6
-macaddr_acl=0
-auth_algs=1
-ignore_broadcast_ssid=0
-wpa=3
+#hw_mode=g
+channel=1
+#macaddr_acl=0
+#auth_algs=1
+#ignore_broadcast_ssid=0
+wpa=2
 wpa_passphrase=KaaVLievaC
 wpa_key_mgmt=WPA-PSK
-wpa_pairwise=TKIP
+#wpa_pairwise=TKIP
 rsn_pairwise=CCMP
+wpa_group_rekey=600
+wpa_gmk_rekey=86400
 EOF
 echo "fin"
 echo "conf ip"
 # Setup the interface
-ip link set lo down
-ip addr flush dev lo
-ip link set lo up
-ip addr add 10.10.10.5/24 dev wlp1s0
+#ip link set lo down
+#ip addr flush dev lo
+#ip link set lo up
+#ip addr add 10.10.10.5/24 dev wlp1s0
 echo "fin"
--- paquetesInstall.sh
+++ paquetesInstall.sh
@@ -2,5 +2,7 @@
 echo "intalando paquetes"
 #apt-get install -y hostapd dnsmasq wireless-tools iw wvdial
 #apt-get update 
+apt-get install wireless-tools iw wvdial
+apt-get install iptables
 apt-get install hostapd
 apt-get install dnsmasq
